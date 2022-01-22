1cbddbffdc1e7fa9ad4433499fa101d6c6533110
lucadonnoh <donnoh99@gmail.com>
2022-01-22 19:05:40 +0100

    fixed library name
    Co-authored-by: Samuele Marro <marrosamuele@gmail.com>

--- src/MetadataUtils.sol
+++ src/MetadataUtils.sol
@@ -5,7 +5,7 @@ pragma solidity >=0.6.0;
 /// @title Base64
 /// @author Brecht Devos - <brecht@loopring.org>
 /// @notice Provides functions for encoding/decoding base64
-library MetadataUtils {
+library Base64 {
     string internal constant TABLE_ENCODE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
     bytes  internal constant TABLE_DECODE = hex"0000000000000000000000000000000000000000000000000000000000000000"
                                             hex"00000000000000000000003e0000003f3435363738393a3b3c3d000000000000"
